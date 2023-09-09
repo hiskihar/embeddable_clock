bb1be1ea67b9d356d513ebf9ac5e5a51574e23fc
hiskihar <114490372+hiskihar@users.noreply.github.com>
2023-09-09 19:35:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-// script.js
+
+
+
+
+
 function updateClock() {
+
     const now = new Date();
+
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    document.getElementById('clock').textContent = timeString;
+
+
+
+    document.getElementById('hours'  ).textContent = `${hours  }`;
+    document.getElementById('minutes').textContent = `${minutes}`;
+    document.getElementById('seconds').textContent = `${seconds}`;
 }
 
 setInterval(updateClock, 1000);
